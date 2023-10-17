de01fd70a622ce62be1817e014219143585ef913
chrischesley <142946987+chrischesley@users.noreply.github.com>
2023-10-17 13:41:32 -0400

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -30,7 +30,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'file:///C:/Users/chris/OneDrive/Documents/Resume%20CMChesley%20c.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'PHP',
